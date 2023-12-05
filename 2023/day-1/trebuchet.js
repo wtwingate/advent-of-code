439227029cf93a5974ee6f9235a999d27df51587
William Wingate <wtwingate@gmail.com>
2023-12-05 02:40:36 -0500

    Almost finished with part 2 of day 1
    What's frustrating is that my program solves for the test data, but
    doesn't work for the real data. The problem is with overlapping regexes.
    It seems like JavaScript has a way to deal with those, but I'm too tired
    to figure that out tonight.

--- 2023/day-1/trebuchet.js
+++ 2023/day-1/trebuchet.js
@@ -1,10 +1,43 @@
 const fs = require("fs")
 
-const lines = fs.readFileSync("data", "utf-8").split("\n");
-const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const lines = fs.readFileSync("alt-data", "utf-8").split("\n");
+const digits = [
+	 "1", "2", "3", "4", "5", "6", "7", "8", "9",
+	 "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+]
 
-calVal = 0;
+const regex = new RegExp(digits.join("|"), "g");
 
+let calVal = 0.0;
+
+lines.forEach((line) => {
+	const digits = [...line.matchAll(regex)];
+	const rawCal = digits[0] + " " + digits[digits.length - 1];
+	const rawArr = rawCal.split(" ");
+	const cookArr = rawArr.map((num) => {
+		if (isNaN(num)) {
+			switch (num) {
+				case "one": return "1";
+				case "two": return "2";
+				case "three": return "3";
+				case "four": return "4";
+				case "five": return "5";
+				case "six": return "6";
+				case "seven": return "7";
+				case "eight": return "8";
+				case "nine": return "9";
+			}
+		} else {
+			return num;
+		}
+	})
+	const cookCal = cookArr.join(""); 
+	console.log(cookCal);
+	calVal += Number(cookCal);
+})
+console.log(calVal);
+
+/*
 lines.forEach((line) => {
 	const arr = line.split("");
 	const arrDigits = arr.filter((char) => {
@@ -15,5 +48,4 @@ lines.forEach((line) => {
 		calVal += Number(cal);
 	}
 })
-
-console.log(calVal);
+*/
