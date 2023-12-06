d5b16e4e8512bdf7f4bc10c4e89735715a4dc470
William Wingate <wtwingate@gmail.com>
2023-12-06 13:49:13 -0500

    Getting closer to a solution for part 1

--- 2023/day-2/index.js
+++ 2023/day-2/index.js
@@ -1,5 +1,3 @@
-const file = require("fs").readFileSync("test-data", "utf-8");
-
 class game {
 	constructor(gameID) {
 		this.gameID = gameID;
@@ -7,25 +5,72 @@ class game {
 	rounds = []
 }
 
+class round {
+	red = 0
+	green = 0
+	blue = 0
+}
+
 // Process line of input into useable data object
 function getGameObject(line) {
 	if (line.length > 0) {
 		const arr = line.split(/:|;/);
 		let gameObj = new game(arr[0]);
+		let roundObj = new round;
 		for (i = 1; i < arr.length; i++) {
-			let round = arr[i].trim();
-			let results = round.split(",");
-			gameObj.rounds.push(results.map((e) => e.trim()));
+			roundObj.red = 0;
+			roundObj.green = 0;
+			roundObj.blue = 0;
+			const round = arr[i].trim();
+			const results = round.split(",");
+			results.forEach((result) => {
+				if (result.includes("red")) {
+					roundObj.red = Number(result.match(/\d+/))
+				}
+				if (result.includes("green")) {
+					roundObj.green = Number(result.match(/\d+/))
+				}
+				if (result.includes("blue")) {
+					roundObj.blue = Number(result.match(/\d+/))
+				}
+			})
+			gameObj.rounds.push(roundObj);
 		}
 		games.push(gameObj);
 	}
 }
 
+const MAXRED = 12;
+const MAXGREEN = 13
+const MAXBLUE = 14
+
 let games = []
+const file = require("fs").readFileSync("test-data", "utf-8");
 const lines = file.split("\n");
 lines.forEach((line) => {
 	getGameObject(line);
 });
 
+let total = 0;
+games.forEach((game) => {
+	let validGame = true;
+	game.rounds.forEach((round) => {
+		if (round.red > MAXRED) {
+			validGame = false;
+		}
+		if (round.green > MAXGREEN) {
+			validGame = false;
+		}
+		if (round.blue > MAXBLUE) {
+			validGame = false;
+		}
+	})
+	if (validGame === true) {
+		total += Number(game.gameID.match(/\d+/));
+	} else {
+		total += 0;
+	}
+})
+
 console.table(games);
-console.table(games[0]);
+console.log(total);
