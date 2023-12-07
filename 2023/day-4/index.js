ae52fa51a0b5f395010f6ff1574a1015522a3f97
William Wingate <wtwingate@gmail.com>
2023-12-07 17:12:17 -0500

    Solve part 1 of day 4
    This part was pretty easy. I'm becoming much more comfortable with
    JavaScript objects, arrays, and strings. I can see why folks say that
    Advent of Code is a great way to learn a language.

--- 2023/day-4/index.js
+++ 2023/day-4/index.js
@@ -1,8 +1,7 @@
 const fs = require("fs");
-const file = fs.readFileSync("example.txt", "utf-8");
+const file = fs.readFileSync("data.txt", "utf-8");
 
-const lines = file.trim().split("\n")
-console.log(lines)
+const lines = file.trim().split("\n");
 
 class card {
 	constructor(cardID) {
@@ -10,13 +9,35 @@ class card {
 	}
 	winningNumbers = [];
 	playerNumbers = [];
+	getScore = function() {
+		let score = 0;
+		this.playerNumbers.forEach((number) => {
+			if (this.winningNumbers.includes(number)) {
+				if (score === 0) {
+					score += 1;
+				} else {
+					score *= 2;
+				}
+			}
+		})
+		return score;
+	}
 }
+
+const deck = [];
 
 lines.forEach((line) => {
 	const cardObj = new card(line.split(":")[0].match(/\d+/)[0]);
 	const winningNumbers = line.split(/:|\|/g)[1];
-	cardObj.winningNumbers = winningNumbers.trim().split(" ");
+	cardObj.winningNumbers = winningNumbers.trim().split(/\s+/);
 	const playerNumbers = line.split(/:|\|/g)[2];
-	cardObj.playerNumbers = playerNumbers.trim().split(" ");
-	console.log(cardObj);
+	cardObj.playerNumbers = playerNumbers.trim().split(/\s+/);
+	deck.push(cardObj);
 })
+
+let totalScore = 0;
+deck.forEach((card) => {
+	totalScore += card.getScore();
+})
+
+console.log(totalScore);
