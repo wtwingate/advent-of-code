d2a496847ee3d364e194a745449e726bf07bf256
William Wingate <wtwingate@gmail.com>
2023-12-07 00:18:29 -0500

    Rewrite index.js to simplify things

--- 2023/day-2/index.js
+++ 2023/day-2/index.js
@@ -5,72 +5,28 @@ class game {
 	rounds = []
 }
 
-class round {
-	red = 0
-	green = 0
-	blue = 0
-}
+// Get lines from data
+const fs = require("fs");
+const file = fs.readFileSync("test-data", "utf-8");
+const rawLines = file.split("\n");
+const lines = rawLines.filter(rawLine => rawLine.trim());
 
-// Process line of input into useable data object
-function getGameObject(line) {
-	if (line.length > 0) {
-		const arr = line.split(/:|;/);
-		let gameObj = new game(arr[0]);
-		let roundObj = new round;
-		for (i = 1; i < arr.length; i++) {
-			roundObj.red = 0;
-			roundObj.green = 0;
-			roundObj.blue = 0;
-			const round = arr[i].trim();
-			const results = round.split(",");
-			results.forEach((result) => {
-				if (result.includes("red")) {
-					roundObj.red = Number(result.match(/\d+/))
-				}
-				if (result.includes("green")) {
-					roundObj.green = Number(result.match(/\d+/))
-				}
-				if (result.includes("blue")) {
-					roundObj.blue = Number(result.match(/\d+/))
-				}
-			})
-			gameObj.rounds.push(roundObj);
-		}
-		games.push(gameObj);
-	}
-}
+const games = [];
 
-const MAXRED = 12;
-const MAXGREEN = 13
-const MAXBLUE = 14
-
-let games = []
-const file = require("fs").readFileSync("test-data", "utf-8");
-const lines = file.split("\n");
+// Parse lines into useable data
 lines.forEach((line) => {
-	getGameObject(line);
-});
-
-let total = 0;
-games.forEach((game) => {
-	let validGame = true;
-	game.rounds.forEach((round) => {
-		if (round.red > MAXRED) {
-			validGame = false;
-		}
-		if (round.green > MAXGREEN) {
-			validGame = false;
-		}
-		if (round.blue > MAXBLUE) {
-			validGame = false;
-		}
+	const arr = line.split(": ");
+	const rounds = arr[1].split("; ");
+	const gameObj = new game(arr[0]);
+	rounds.forEach((round) => {
+		const roundScores = [];
+		const scores = round.split(", ");
+		roundScores.push(scores);
+		gameObj.rounds.push(roundScores);
 	})
-	if (validGame === true) {
-		total += Number(game.gameID.match(/\d+/));
-	} else {
-		total += 0;
-	}
+	games.push(gameObj);
 })
 
-console.table(games);
-console.log(total);
+games.forEach((game) => {
+	game.rounds.forEach(round => console.table(round));
+})
