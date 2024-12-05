91b39f77f0ff993e00810ec4b3deaa6274b13f57
William Wingate <wtwingate@gmail.com>
2024-12-04 20:53:18 -0500

    Format Python code with Ruff

--- 2024/02_red_nosed_reports/reports.py
+++ 2024/02_red_nosed_reports/reports.py
@@ -1,18 +1,18 @@
 def dampened_report_safe(dampened):
     prev_num = None
     delta = dampened[1] - dampened[0]
-    
+
     for num in dampened:
         if prev_num is None:
             prev_num = num
             continue
-        
+
         diff = num - prev_num
         if abs(diff) < 1 or abs(diff) > 3:
             return False
         if diff * delta < 0:
             return False
-        
+
         prev_num = num
 
     return True
@@ -20,11 +20,11 @@ def dampened_report_safe(dampened):
 
 def report_safe(report):
     for i, _ in enumerate(report):
-        dampened = report[:i] + report[i + 1:]
+        dampened = report[:i] + report[i + 1 :]
         if dampened_report_safe(dampened):
             return True
     return False
-    
+
 
 def count_safe_reports(filename):
     reports = []
--- 2024/03_mull_it_over/mull.py
+++ 2024/03_mull_it_over/mull.py
@@ -3,6 +3,7 @@ import re
 with open("input.txt") as f:
     data = f.read().strip()
 
+
 def sum_of_muls(data: str) -> int:
     muls = re.findall(r"mul\((\d+),(\d+)\)", data)
 
@@ -12,9 +13,10 @@ def sum_of_muls(data: str) -> int:
 
     return sum
 
+
 def remove_dont_blocks(data: str) -> str:
     blocks = re.split(r"(do\(\)|don't\(\))", data)
-    
+
     do = ""
     for i in range(len(blocks)):
         if i == 0:
@@ -25,5 +27,6 @@ def remove_dont_blocks(data: str) -> str:
             do += blocks[i]
 
     return do
+
 
 sum_of_muls(remove_dont_blocks(data))
